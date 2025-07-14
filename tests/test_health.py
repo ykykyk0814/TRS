69d5f2d7ed766e3eeb30924f3683abcccbdbbf7f
Yakun <yd2023@nyu.edu>
2025-07-14 12:53:03 -0400

    test: add working health check test

--- tests/test_health.py
+++ tests/test_health.py
@@ -4,7 +4,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_health():
+def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
